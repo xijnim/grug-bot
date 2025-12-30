b01edb97c7c67887d07095b673389a4c3659f929
xijnim <xijnin@gmail.com>
2025-12-30 14:43:55 -0500

    Integrated the grug stdlib

--- grug_eval.py
+++ grug_eval.py
@@ -28,24 +28,18 @@ with open("mods/THE_MOD/about.json", "w", encoding="utf-8") as f:
 with open("mods/THE_MOD/code-Script.grug", "w", encoding="utf-8") as f:
     f.write(code)
 
+from grug.packages import grug_stdlib, grug_numpy
 
-state = grug.init()
+state = grug.init(
+    packages=[
+        grug_stdlib.get().noprefix(),
+        grug_numpy.get().noprefix(),
+    ]
+)
 
 @state.game_fn
 def print_string(code: str):
     print(code)
-
-@state.game_fn
-def number_to_string(code: float) -> str:
-    return str(code)
-
-@state.game_fn
-def print_number(code: float):
-    print(code)
-
-@state.game_fn
-def ceil(n: float) -> float:
-    return math.ceil(n)
 
 try:
     file = state.compile_grug_file("THE_MOD/code-Script.grug")
